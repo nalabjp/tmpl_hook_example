86915fa3c33c725dea8df8161e0b4ec80742a044
nalabjp <nalabjp@gmail.com>
2016-08-06 03:24:47 +0900

    :sparkles: Create route 'top#slim'

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
   def haml
   end
+  def slim
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'top/haml'
+  get 'top/slim'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
